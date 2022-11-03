700f5a93d2fdef66dc5efce442e97375cd10ae78
root <Param>
2022-11-03 21:47:57 +0000

    Chef Resources

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['lgmuser']['user'] = 'lgmuser'
+default['lgmuser']['group'] = 'lgmuser'
+default['lgmuser']['url'] = 'http://www.sbeams.org/sample_data/Microarray/External_test_data.tar.gz'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,17 +1,42 @@
-#
-# Cookbook:: ihg_lmconfig
-# Recipe:: default
-#
-# Copyright:: 2022, The Authors, All Rights Reserved.
-script 'install_something' do
-  interpreter 'bash'
-  user 'root'
-  cwd '/tmp'
-  code <<-EOH
-    id -u lgmuser1 2>&1 >/dev/null && echo "user lgmuser1 exists" || useradd -r -m lgmuser1 -c "LogicMonitor local linux ssh account"
-    su - lgmuser1
-    wget http://www.sbeams.org/sample_data/Microarray/External_test_data.tar.gz
-    tar xvfz External_test_data.tar.gz
-  EOH
+luser = node['lgmuser']['user']
+lgroup = node['lgmuser']['group']
+lurl = node['lgmuser']['url']
+
+group lgroup do
+  gid 1234
+  group_name lgroup
+  not_if "getent group #{lgroup}"
 end
 
+directory "/home/#{luser}" do
+  owner luser
+  group lgroup
+  mode '0755'
+  action :create
+end
+
+user luser do
+  comment 'LogicMonitor local linux ssh account'
+  uid 1234
+  username luser
+  gid lgroup
+  home "/home/#{luser}"
+  shell '/bin/bash'
+  not_if "getent passwd #{luser}"
+end
+
+remote_file '/tmp/logicmonitor_ihg_ssh.tgz' do
+  source lurl
+  user   luser
+  group  lgroup
+  mode   '0444'
+  action :create
+end
+
+archive_file 'logicmonitor_ihg_ssh.tgz' do
+  owner luser
+  group lgroup
+  mode '700'
+  path '/tmp/logicmonitor_ihg_ssh.tgz'
+  destination "/home/#{luser}"
+end
